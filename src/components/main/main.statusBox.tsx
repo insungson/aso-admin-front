599dde29c78962362e75f29c486c3f3e9a19ab76
Sonny <sis_0101@coresight.io>
2023-08-28 18:16:22 +0900

    add

--- src/components/main/main.statusBox.tsx
+++ src/components/main/main.statusBox.tsx
@@ -9,24 +9,40 @@ const UserStatusBox = () => {
         <div className="main-nav-left">
           <header>사용자 현황</header>
           <div className="half_module">
-            <ul className="appinfo_statelist">
-              <li>
-                <span className="appinfo_statelist_span">11</span>
-                <strong className="appinfo_statelist_strong">123</strong>
-              </li>
-              <li>
-                <span className="appinfo_statelist_span">11</span>
-                <strong className="appinfo_statelist_strong">123</strong>
-              </li>
-              <li>
-                <span className="appinfo_statelist_span">11</span>
-                <strong className="appinfo_statelist_strong">123</strong>
-              </li>
-              <li>
-                <span className="appinfo_statelist_span">11</span>
-                <strong className="appinfo_statelist_strong">123</strong>
-              </li>
-            </ul>
+            {!!userStatus && (
+              <ul className="appinfo_statelist">
+                <li>
+                  <span className="appinfo_statelist_span">
+                    {userStatus?.dau}
+                  </span>
+                  <strong className="appinfo_statelist_strong">DAU</strong>
+                </li>
+                <li>
+                  <span className="appinfo_statelist_span">
+                    {userStatus?.experiencedUser}
+                  </span>
+                  <strong className="appinfo_statelist_strong">
+                    체험 사용자
+                  </strong>
+                </li>
+                <li>
+                  <span className="appinfo_statelist_span">
+                    {userStatus?.freeUser}
+                  </span>
+                  <strong className="appinfo_statelist_strong">
+                    무료 사용자
+                  </strong>
+                </li>
+                <li>
+                  <span className="appinfo_statelist_span">
+                    {userStatus?.paidUser}
+                  </span>
+                  <strong className="appinfo_statelist_strong">
+                    유료 사용자
+                  </strong>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       )}
